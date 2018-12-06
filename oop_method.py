589dd9b0d62bee90c87c43ffcfc691b11991429b
Mandy Tao <taoxiumin2016@gmail.com>
2018-12-06 14:59:34 +0800

    面向对象编程

---
+++ oop_method.py
@@ -0,0 +1,7 @@
+class Person:
+    def say_hi(self):
+        print('Hello, how are you?')
+
+
+p = Person() # 创建实例
+p.say_hi()
---
+++ oop_simplestclass.py
@@ -0,0 +1,5 @@
+class Person:
+    pass # 一个空的代码块
+
+p = Person() # 创建实例
+print(p)
